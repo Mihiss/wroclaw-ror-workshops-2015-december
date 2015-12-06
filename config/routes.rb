ea9ae59027ed8a65ddc249f2b9f69d4973cdbf1c
Mihiss <mihi24@gmail.com>
2015-12-06 22:04:45 +0100

    fix user and env

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :students do
     get :subjects
   end
 
   root to: 'visitors#index'
 
-  get 'students' => 'students#index'
-  get 'teachers' => 'teachers#index'
   get 'report/subjects' => 'reports#_subject_item'
+  get 'visitors', to: 'visitors#index'
 end
